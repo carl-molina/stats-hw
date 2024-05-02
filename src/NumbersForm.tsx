ebc357e8b5399f9a0acb2fed00052646b9b9d832
Carl Molina <carlandrew.molina@gmail.com>
2024-05-02 11:39:51 -0700

    Added TODO comment

--- src/NumbersForm.tsx
+++ src/NumbersForm.tsx
@@ -1,2 +1,8 @@
 "use strict";
 
+// TODO: create a component that takes in an input from a textarea box. The
+// input should be numbers with a single space in between (note: they will be in
+// type string when you split them, which means you'll need to convert them to
+// type number before doing any calculuations. After you've successfully split
+// the numbers and converted them, you can then do your statistical
+// calculations.)
