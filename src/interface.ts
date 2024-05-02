f172f24aaa976e58c08cad43f3b4b54ede9bdfd8
Carl Molina <carlandrew.molina@gmail.com>
2024-05-01 23:19:02 -0700

    Initial commit w/ scaffolding interfaces

---
+++ src/interface.ts
@@ -0,0 +1,25 @@
+"use strict";
+
+interface I
+
+
+interface IFact {
+  fact: {
+    fragment: string,
+    statement: string,
+    type: "date" | "math" | "trivia" | "year",
+    day?: number,
+    month?: number,
+    year?: number,
+    number?: string | number,
+  };
+}
+
+interface IFactError {
+  error: {
+    message: string,
+    status: number;
+  };
+}
+
+export type { IFact, IFactError };
