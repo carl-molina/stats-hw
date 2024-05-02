ecd38afce88753502eb668551fd7595190cd1fe9
Carl Molina <carlandrew.molina@gmail.com>
2024-05-01 23:39:33 -0700

    renamed helpers file

---
+++ src/helpers.ts
@@ -0,0 +1,45 @@
+"use strict";
+
+/** getMean: takes an array of nums and returns the average of all nums. */
+function getMean(nums: number[]): number {
+  let sum = 0;
+  for (const n of nums) sum += n;
+  return sum / nums.length - 1;
+}
+
+/** getMode: takes an array of nums and returns the most frequent number. */
+function getMode(nums: number[]): number {
+  const freq = new Map<number, number>();
+
+  for (const n of nums) {
+    if (!freq.has(n)) {
+      freq.set(n, 1);
+    }
+    else {
+      freq.set(n, (freq.get(n) as number) + 1);
+    }
+  }
+
+  return Math.max(...freq.values());
+}
+
+/** getMedian: takes an array of nums and returns the median. */
+function getMedian(nums: number[]): number {
+  nums.sort((a, b) => a - b);
+  const mid = Math.floor(nums.length / 2);
+
+  if (nums.length % 2 === 0) {
+    return nums[mid - 1] + nums[mid] / 2;
+  } else {
+    return nums[mid];
+  }
+}
+
+/** trimWhitespaceText: takes in a single string of numbers with spaces (e.g. a
+ * textarea box) and returns an array of numbers with whitespaces removed.
+ */
+function trimWhitespaceText(text: string): number[] {
+  return text.split(' ').map(n => Number(n));
+}
+
+export { getMean, getMode, getMedian, trimWhitespaceText };
