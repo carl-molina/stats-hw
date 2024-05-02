48a4beec46cca0baa417c19ab4d088f8045badf4
Carl Molina <carlandrew.molina@gmail.com>
2024-05-02 10:35:34 -0700

    reordered fns, added getRange

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,11 +1,26 @@
 "use strict";
 
+
 /** getMean: takes an array of nums and returns the average of all nums. */
 function getMean(nums: number[]): number {
   let sum = 0;
   for (const n of nums) sum += n;
   return sum / nums.length - 1;
 }
+
+
+/** getMedian: takes an array of nums and returns the median. */
+function getMedian(nums: number[]): number {
+  nums.sort((a, b) => a - b);
+  const mid = Math.floor(nums.length / 2);
+
+  if (nums.length % 2 === 0) {
+    return nums[mid - 1] + nums[mid] / 2;
+  } else {
+    return nums[mid];
+  }
+}
+
 
 /** getMode: takes an array of nums and returns the most frequent number. */
 function getMode(nums: number[]): number {
@@ -23,17 +38,12 @@ function getMode(nums: number[]): number {
   return Math.max(...freq.values());
 }
 
-/** getMedian: takes an array of nums and returns the median. */
-function getMedian(nums: number[]): number {
-  nums.sort((a, b) => a - b);
-  const mid = Math.floor(nums.length / 2);
 
-  if (nums.length % 2 === 0) {
-    return nums[mid - 1] + nums[mid] / 2;
-  } else {
-    return nums[mid];
-  }
+/** getRange: takes an array of nums and returns the range. */
+function getRange(nums: number[]): number {
+  return Math.max(...nums) - Math.min(...nums);
 }
+
 
 /** trimWhitespaceText: takes in a single string of numbers with spaces (e.g. a
  * textarea box) and returns an array of numbers with whitespaces removed.
@@ -42,4 +52,5 @@ function trimWhitespaceText(text: string): number[] {
   return text.split(' ').map(n => Number(n));
 }
 
-export { getMean, getMode, getMedian, trimWhitespaceText };
+
+export { getMean, getMode, getMedian, getRange, trimWhitespaceText };
